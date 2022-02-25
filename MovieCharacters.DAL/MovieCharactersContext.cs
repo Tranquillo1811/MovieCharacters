9e9b7084764147f4fbee34786657a080ea0a7085
Tranquillo1811 <oliver.hauck@de.experis.com>
2022-02-25 16:48:39 +0100

    INIT: sets up the project structure

---
+++ MovieCharacters.DAL/MovieCharactersContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieCharacters.DAL
+{
+    internal class MovieCharactersContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseSqlServer("Data Source = localhost; Initial Catalog = MovieCharactersDb; Integrated Security = True; Trust Server Certificate = True");
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,29 @@
+# ob-oh-Pokemon-Trainer
+
+very simple Movie Characters App soleloy built for educational purposes
+- uses .NET Core 5
+- uses Entity Framework
+
+
+# Install
+ 
+[Visual Studio Code](https://code.visualstudio.com/download)
+
+
+# Usage
+
+dotnet run
+
+
+# Maintainers
+<https://github.com/Tranquillo1811> 
+
+<https://github.com/OBeratis>
+
+# Contributing
+This project is not supposed to have any contributors.
+It is supposed to grade the individuals who composed it.
+Right after that graduation the project will be - most likely - instantly discontinued...
+
+# License
+[Apache License &copy; 2022](./LICENSE)
