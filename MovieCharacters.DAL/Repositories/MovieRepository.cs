3b5c1e07e4418b43d8ae3f50fa731afb855cfbd3
Tranquillo1811 <oliver.hauck@de.experis.com>
2022-03-03 09:57:58 +0100

    implements methods in MovieRepository

--- MovieCharacters.DAL/Repositories/MovieRepository.cs
+++ MovieCharacters.DAL/Repositories/MovieRepository.cs
@@ -1,23 +1,59 @@
-﻿using MovieCharacters.BLL.Models;
+﻿using Microsoft.EntityFrameworkCore;
+using MovieCharacters.BLL.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace MovieCharacters.DAL.Repositories
 {
     public class MovieRepository : IMovieRepository
     {
+        /// <summary>
+        /// adds a movie to the Db
+        /// </summary>
+        /// <param name="entity">Movie to be added to the Db</param>
+        /// <returns>newly added movie</returns>
         public async Task<Movie> AddAsync(Movie entity)
         {
-            throw new NotImplementedException();
+            Movie movieResult = null;
+            using (MovieCharactersContext context = new())
+            {
+                try
+                {
+                    movieResult = (await context.Movies.AddAsync(entity)).Entity;
+                    int intResult = await context.SaveChangesAsync();
+                }
+                catch (Exception ex)
+                {
+                    //TODO: not quite sure, how to actually handle this...
+                }
+            }
+            return movieResult;
         }
 
-        public async Task<int> DeleteByIdAsync(int entityId)
+        /// <summary>
+        /// deletes Movie with respective Id
+        /// </summary>
+        /// <param name="MovieId">id of the movie to be deleted</param>
+        /// <returns>id of the deleted movie</returns>
+        public async Task<int> DeleteByIdAsync(int MovieId)
         {
-            throw new NotImplementedException();
+            Movie deleteMovie = await GetByIdAsync(MovieId);
+            using (MovieCharactersContext context = new())
+            {
+                try
+                {
+                    context.Movies.Remove(deleteMovie);
+                    await context.SaveChangesAsync();
+                }
+                catch (Exception ex)
+                {
+                    //TODO: not quite sure, how to actually handle this...
+                }
+            }
+            return MovieId;
         }
 
         public async Task<IEnumerable<Movie>> FindAllAsync(Expression<Func<Movie, bool>> predicate)
@@ -25,19 +61,74 @@ namespace MovieCharacters.DAL.Repositories
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// gets all movies from Db
+        /// </summary>
+        /// <returns>List of all movies from Movies Db table</returns>
         public async Task<IEnumerable<Movie>> GetAllAsync()
         {
-            throw new NotImplementedException();
+            List<Movie> movies = new();
+            using (MovieCharactersContext context = new())
+            {
+                try
+                {
+                    movies = await context.Movies.Cast<Movie>().ToListAsync();
+                }
+                catch (Exception ex)
+                {
+                    //TODO: not quite sure, how to actually handle this...
+                }
+            }
+            return movies;
         }
 
+        /// <summary>
+        /// get movie with particular Id from Movies table
+        /// </summary>
+        /// <param name="id">id of the movie to be selected</param>
+        /// <returns>movie with given id or null if no such movie exists</returns>
         public async Task<Movie> GetByIdAsync(int id)
         {
-            throw new NotImplementedException();
+            Movie movie = null;
+            using (MovieCharactersContext context = new())
+            {
+                try
+                {
+                    movie = await context.Movies.FindAsync(id);
+                }
+                catch (Exception ex)
+                {
+                    //TODO: not quite sure, how to actually handle this...
+                }
+            }
+            return movie;
         }
 
+        /// <summary>
+        /// updates a movie in Movies Db table
+        /// </summary>
+        /// <param name="entity">movie to be updated</param>
+        /// <returns>updated movie</returns>
         public async Task<Movie> UpdateAsync(Movie entity)
         {
-            throw new NotImplementedException();
+            Movie movieResult;
+            using (MovieCharactersContext context = new())
+            {
+                context.Entry(entity).State = EntityState.Modified;
+                int intResult = 0;
+                try
+                {
+                    intResult = await context.SaveChangesAsync();
+                }
+                catch (Exception ex)
+                {
+                    //TODO: not quite sure, how to actually handle this...
+                }
+                if (intResult == 0)
+                    return null;
+                movieResult = entity;
+            }
+            return movieResult;
         }
     }
 }
